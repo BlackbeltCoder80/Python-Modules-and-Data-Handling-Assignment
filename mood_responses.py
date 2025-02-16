16a5ea162e59780bc6f4f804f76c0ddaaa4a0ae5
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-02-16 01:27:04 -0700

    Updated

---
+++ mood_responses.py
@@ -0,0 +1,9 @@
+def mood_response(mood):
+    if mood == "happy":
+        return "I'm excited that you are happy today."
+    elif mood == "sad":
+        return "I'm here for you, even in tough times."
+    elif mood == "angry":
+        return "Take a deep breath, and let's talk it out."
+    else:
+        return "That's an interesting mood! Tell me more."
