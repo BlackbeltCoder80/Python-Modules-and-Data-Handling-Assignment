38fd90b2834c02458706aae95f160cd6f1163fb9
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-02-17 22:21:27 -0700

    Updated

---
+++ Custom_Modules_Aliases.py
@@ -0,0 +1,23 @@
+#2. Mastering Python Modules and Aliases
+#Objective: The aim of this assignment is to enhance your proficiency in using Python modules, both standard and custom, with a specific focus on importing with aliases.
+
+#Task 1: Custom Module with Aliases 
+
+#Problem Statement: Create a custom module named `text_utils.py` with functions for string manipulation (e.g., reversing a string, capitalizing). In your main script, import this module using an alias and utilize its functions.
+
+#Code Example:
+
+from text_utils import capitalize_string, reverse_string
+
+
+text = "hello"
+print(capitalize_string(text))
+print(reverse_string(text))
+
+    # main.py
+    # Import text_utils using an alias and utilize its functions
+#- Expected Outcome: Your main script should be able to use the functions from `text_utils.py` via an alias, demonstrating an understanding of custom module creation and aliasing.
+
+#NOTE: Ensure that all code in your file is ready to run. This means that if someone opens your file and clicks the run button at the top, all code executes as intended. For example, if there are if statements, print statements, or for loops, they should function correctly and display output in the console as expected. If you have a function, make sure the function is called and runs.
+
+#The goal of this note is to ensure that all code in your Python file runs smoothly and that is has been tested.
